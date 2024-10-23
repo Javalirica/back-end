f9841bd75bd8d7a01270f2375461a737d1371e5c
leonardogabriels <leonardogabrielsilva1@outlook.com>
2024-10-23 14:40:11 -0300

    first commit

---
+++ src/main/java/br/com/javalirica/JavaliricaBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.javalirica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaliricaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaliricaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javalirica/JavaliricaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javalirica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaliricaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
